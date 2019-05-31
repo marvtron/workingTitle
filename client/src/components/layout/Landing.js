0f8349440e17292ea39ab64f661490ab75327a96
Marv_Tron <marvinatryonjr@gmail.com>
2019-05-30 18:50:25 -0700

    vetCard component && Additons to landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -39,6 +39,23 @@ class Landing extends Component {
               Log In
             </a>
             <p className="flow-text grey-text text-darken-1">
+            <Card style={{ width: '100%', fontFamily: "Monospace", fontStyle:"italic" }}>
+              <Card.Body>
+                <Card.Title style={{fontWeight:'bold', color:'black'}}>"something.. somrhing ..someting"</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                <Card.Text>
+                <span>For veterans...
+
+                ..Feeling of Love and Closeness..
+                ..Improving Trust..
+                ..Overcoming Fears..
+                ..Getting Into a Routine..
+                </span>
+                <br></br>
+                </Card.Text>
+                <Card.Link href="https://www.legion.org/veteranshealthcare/203662/taking-d-out-ptsd" target="_blank">Additional Info</Card.Link>
+              </Card.Body>
+            </Card>
             <Card style={{ width: '100%', marginTop:'30px', fontFamily: "Monospace", fontStyle:"italic" }}>
               <Card.Body>
                 <Card.Title style={{fontWeight:'bold', color:'black'}}>"What is Equine Therapy?"</Card.Title>
@@ -46,9 +63,13 @@ class Landing extends Component {
                 <Card.Text>
                 <span>Equine Therapy (also referred to as Horse Therapy, Equine-Assisted Therapy, and Equine-Assisted Psychotherapy) is a form of experiential therapy that involves interactions between patients and horses.</span>
                 <br></br>
-                <span>Equine Therapy involves activities (such as grooming, feeding, haltering and leading a horse) that are supervised by a mental health professional, often with the support of a horse professional.</span>
+                <span>Horses are naturally intuitive and sensitive animals capable of sensing their riders’ moods and mirroring their behavior. 
+                  This is what makes them such excellent therapeutic animals. Therapeutic horse riding has been recognized as beneficial in helping individuals suffering from various mental, physical and emotional health issues to grow and heal.
+                   This includes those with PTSD, autism, cerebral palsy, among others
+                   <br></br>
+                  Equine Therapy involves activities (such as grooming, feeding, haltering and leading a horse) that are supervised by a mental health professional, often with the support of a horse professional.</span>
                 </Card.Text>
-                <Card.Link href="https://www.crchealth.com/types-of-therapy/what-is-equine-therapy/">Additional Info</Card.Link>
+                <Card.Link href="https://www.crchealth.com/types-of-therapy/what-is-equine-therapy/" target="_blank">Additional Info</Card.Link>
               </Card.Body>
             </Card>
             <Card style={{ width: '100%', fontFamily: "Monospace", fontStyle:"italic" }}>
@@ -56,10 +77,13 @@ class Landing extends Component {
                 <Card.Title style={{fontWeight:'bold', color:'black'}}>"Take the 'D' out of PTSD"</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Dr. Jeanne Stellman of Columbia University</Card.Subtitle>
                 <Card.Text>
-                <span>"Many people think our entire group of veterans are all nutsy because they have been exposed to combat. But potential employers don’t know their personal medical history," and can’t know for sure if a veteran has PTSD or not. "We need to overcome the stigma by education and figuring out the realistic alternatives."</span>
+                <span>"Many people think our entire group of veterans are all nutsy because they have been exposed to combat. 
+                  But potential employers don’t know their personal medical history," and can’t know for sure if a veteran has PTSD or not. 
+                  "We need to overcome the stigma by education and figuring out the realistic alternatives."
+                  It isn't a disorder if everyone experiences it.</span>
                 <br></br>
                 </Card.Text>
-                <Card.Link href="https://www.legion.org/veteranshealthcare/203662/taking-d-out-ptsd">Additional Info</Card.Link>
+                <Card.Link href="https://www.legion.org/veteranshealthcare/203662/taking-d-out-ptsd" target="_blank">Additional Info</Card.Link>
               </Card.Body>
             </Card>
             <Card style={{ width: '100%', fontFamily: "Monospace", fontStyle:"italic" }}>
@@ -72,7 +96,7 @@ class Landing extends Component {
                 <br></br>
                 <span></span>
                 </Card.Text>
-                <Card.Link href="http://www.operationwearehere.com/EquineTherapy.html">Find someone near you</Card.Link>
+                <Card.Link href="http://www.operationwearehere.com/EquineTherapy.html" target="_blank">Find someone near you</Card.Link>
               </Card.Body>
             </Card>
             </p>
--- client/src/components/layout/Vets.js
+++ client/src/components/layout/Vets.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
+import VetCard from '../../components/VetCard/VetCard';
 import "../../App.css";
 
-class Vets extends Component {
+class Vet extends Component {         
     render() {
-        return(
-            <div className='background2 container'>
-                <div style={{height:'100vh'}}>
-                <h1 style={{textAlign:'center', color:'#eee', marginTop:'30px', fontFamily:'monospace'}}> Talk to a vet</h1>
-                </div>
+          return (
+            <div className='background2'>
+              <div>
+            <VetCard/>
             </div>
-        );
-    }
-}
-
-export default Vets;
+            </div>         
+          );
+        }
+      }    
+  
+    export default Vet;
